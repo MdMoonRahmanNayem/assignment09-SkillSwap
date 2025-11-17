39c78b4268d23cc26335875e4e0ff07ebeee7ff7
MdMoonRahmanNayem <mmrn3182@gmail.com>
2025-11-17 14:25:37 +0600

    create project structure

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <h1 className="text-4xl font-bold text-red-500">Tailwind Working!</h1>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
